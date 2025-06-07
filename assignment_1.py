c58a239fb01d93efa4817098db72a227c9442e5d
Stutisona <stutishrivastava2003@gmail.com>
2025-06-07 20:27:17 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,26 @@
+def lower_triangular(n):
+    print("Lower Triangular Pattern:")
+    for i in range(1, n + 1):
+        print("*" * i)
+    print()
+
+def upper_triangular(n):
+    print("Upper Triangular Pattern:")
+    for i in range(n, 0, -1):
+        spaces = " " * (n - i)
+        stars = "*" * i
+        print(spaces + stars)
+    print()
+
+def pyramid(n):
+    print("Pyramid Pattern:")
+    for i in range(1, n + 1):
+        spaces = " " * (n - i)
+        stars = "*" * (2 * i - 1)
+        print(spaces + stars)
+    print()
+
+rows = 6
+lower_triangular(rows)
+upper_triangular(rows)
+pyramid(rows)
